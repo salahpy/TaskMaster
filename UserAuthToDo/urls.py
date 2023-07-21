d60710d3cbe2dc75ed59e818a3ccaf2ff2d998c0
salahpy <s.nacef@esi-sba.dz>
2023-07-21 04:25:41 +0100

    CRUD added

--- UserAuthToDo/urls.py
+++ UserAuthToDo/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('task.urls')),
+    path('tasks/',include('task.urls')),
 ]
--- task/urls.py
+++ task/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('list/',TaskList.as_view()),
+    path('listCompleted/',TaskListCompleted.as_view()),
+    path('create/',CreateTask.as_view()),
+    path('delete/<int:pk>/', DeleteTask.as_view()),
+    path('update/<int:pk>/', UpdateTask.as_view()),
 ]
